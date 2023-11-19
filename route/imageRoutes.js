b61929fa2b48e8032281d304759e355d1c466e7c
soukainamelhaoui <soukaina.melhaoui@etu.uae.ac.ma>
2023-11-19 15:14:33 +0100

    upload, download, delete, multiple images

--- route/imageRoutes.js
+++ route/imageRoutes.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 const auth = require('../middleware/auth');
 const Image = require('../model/image');
 const router = express.Router();
+const archiver = require('archiver');
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -24,7 +25,7 @@ router.post('/upload', auth, upload.single('image'), async (req, res) => {
 
         const savedImage = await newImg.save();
         res.status(200).json(newImg);
-        //res.status(200).json({ message: "Image uploaded successfully" });
+
     } catch (error) {
         console.error(error);
         res.status(500).send("Internal server error");
@@ -97,6 +98,91 @@ router.get('/download-image/:id', auth, async (req, res) => {
     }
 
 })
+
+router.post('/upload-multiple', auth, upload.array('images'), async (req, res) => {
+    try {
+        const images = [];
+
+        for (const file of req.files) {
+            const metadata = await sharp(file.buffer).metadata();
+
+            const newImg = new Image({
+                filename: file.originalname,
+                contentType: file.mimetype,
+                image: file.buffer,
+                width: metadata.width,
+                height: metadata.height,
+                user: req.user.user_id,
+                category: req.body.categoryId
+            });
+
+            const savedImage = await newImg.save();
+            images.push(savedImage);
+        }
+
+        res.status(200).json(images);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server error");
+    }
+});
+
+router.delete('/delete-images', auth, async (req, res) => {
+    try {
+        const imageIds = req.body.imageIds;
+
+        const isValidObjectIds = await Promise.all(imageIds.map(id => Image.findById(id)));
+
+        if (!isValidObjectIds.every(image => image)) {
+            return res.status(400).json({ message: 'Invalid image IDs' });
+        }
+
+        const deletedImages = await Image.deleteMany({ _id: { $in: imageIds }, user: req.user.user_id });
+
+        res.status(200).json({ message: 'Images deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/download-images', auth, async (req, res) => {
+    try {
+        const imageIds = req.body.imageIds;
+
+        const isValidObjectIds = await Promise.all(imageIds.map(id => Image.findById(id)));
+
+        if (!isValidObjectIds.every(image => image)) {
+            return res.status(400).json({ message: 'Invalid image IDs' });
+        }
+
+        const images = await Image.find({ _id: { $in: imageIds } });
+
+        for (const image of images) {
+            if (image.user.toString() !== req.user.user_id) {
+                return res.status(403).json({ message: 'Access denied. You don’t have permission to download one or more images.' });
+            }
+        }
+
+        const zip = archiver('zip', {
+            zlib: { level: 9 }
+        });
+
+        res.attachment('images.zip');
+        zip.pipe(res);
+
+        for (const image of images) {
+            zip.append(image.image, { name: image.filename });
+        }
+
+        zip.finalize();
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 
 module.exports = router;
